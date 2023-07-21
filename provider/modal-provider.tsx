330300eb2ffa574e2fed2286cee688b6ec0218c9
jawad-nsu <jawadurrahim1997@gmail.com>
2023-07-21 16:35:05 -0400

    this component is meant to keep server and client in sync and avoid hydration error

---
+++ provider/modal-provider.tsx
@@ -0,0 +1,17 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+
+import { StoreModal } from '@/components/modals/storeModal';
+
+export const modalProvider = () => {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) return null;
+
+  return <StoreModal />;
+};
