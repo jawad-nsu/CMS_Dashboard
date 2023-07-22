e0b305aabc53c2097535a47d57389a9adb4e93b4
jawad-nsu <jawadurrahim1997@gmail.com>
2023-07-21 21:27:20 -0400

    render store modal form on startup

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,22 @@
-import { Button } from '@/components/ui/button';
+'use client';
 
-export default function Home() {
-  return (
-    <div className='m-2'>
-      <Button>Bye there</Button>
-    </div>
-  );
+import { useEffect } from 'react';
+
+import { useStoreModal } from '@/hooks/use-store-modals';
+
+import { Button } from '@/components/ui/button';
+import { Modal } from '@/components/ui/modal';
+import { UserButton } from '@clerk/nextjs';
+
+export default function SetupPage() {
+  const isOpen = useStoreModal((state) => state.isOpen);
+  const onOpen = useStoreModal((state) => state.onOpen);
+
+  useEffect(() => {
+    if (!isOpen) {
+      onOpen();
+    }
+  }, [isOpen, onOpen]);
+
+  return <div className='m-2'>Root Page</div>;
 }
