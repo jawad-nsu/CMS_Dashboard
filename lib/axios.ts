d4fccf9107faeb4087f25cd97499461063b3fa0c
jawad-nsu <jawadurrahim1997@gmail.com>
2023-07-24 22:44:48 -0400

    axios wrapper

--- lib/axios.ts
+++ lib/axios.ts
@@ -9,6 +9,7 @@ const axiosInstance = axios.create({
 
 // intercepts request before they're handled by then / catch
 // central error handler
+
 axiosInstance.interceptors.response.use(
   (response) => response,
   (error) =>
