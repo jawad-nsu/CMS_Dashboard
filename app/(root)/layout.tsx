b7daa99769521deeea795af316db6f32b9e3567a
jawad-nsu <jawadurrahim1997@gmail.com>
2023-07-26 22:20:40 -0400

    move export from top of the page to bottom of the page

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -2,20 +2,20 @@ import prismadb from '@/lib/prismadb';
 import { auth } from '@clerk/nextjs';
 import { redirect } from 'next/navigation';
 
-export const SetupPage = async ({
-  children,
-}: {
-  children: React.ReactNode;
-}) => {
+const SetupPage = async ({ children }: { children: React.ReactNode }) => {
   const { userId } = auth();
 
   if (!userId) redirect('/sign-in');
 
   const store = await prismadb.store.findFirst({
-    userId,
+    where: {
+      userId,
+    },
   });
 
   if (store) redirect(`/${store.id}`);
 
   return <div>{children}</div>;
 };
+
+export default SetupPage;
