d6df7b4281f38ff82521a3143cc30a3851f6038e
jawad-nsu <jawadurrahim1997@gmail.com>
2023-07-27 20:45:45 -0400

    Store Settings heading

---
+++ components/ui/heading.tsx
@@ -0,0 +1,15 @@
+interface HeadingProps {
+  title: String;
+  description: String;
+}
+
+const Heading: React.FC<HeadingProps> = ({ title, description }) => {
+  return (
+    <div>
+      <h1 className='text-3xl font-bold tracking-tight'>{title}</h1>
+      <p className='text-sm text-muted-foreground'>{description}</p>
+    </div>
+  );
+};
+
+export default Heading;
