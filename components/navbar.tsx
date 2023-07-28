c7639205606fa7fb72f06da84c3faf70244b3405
jawad-nsu <jawadurrahim1997@gmail.com>
2023-07-27 20:44:51 -0400

    add MainNavbar to navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,12 +1,28 @@
-import { UserButton } from '@clerk/nextjs';
+import { redirect } from 'next/navigation';
+import { UserButton, auth } from '@clerk/nextjs';
+import prismadb from '@/lib/prismadb';
+
+// components
 import MainNav from '@/components/main-nav';
 import StoreSwitcher from '@/components/storeSwitcher';
 
-const Navbar = () => {
+const Navbar = async () => {
+  const { userId } = auth();
+
+  if (!userId) {
+    redirect('sign-in');
+  }
+
+  const stores = await prismadb.store.findMany({
+    where: {
+      userId,
+    },
+  });
+
   return (
     <div className='border-b'>
       <div className='flex h-16 items-center px-4'>
-        <StoreSwitcher />
+        <StoreSwitcher items={stores} />
         <MainNav className='mx-6' />
         <div className='ml-auto flex items-center space-x-4'>
           <UserButton afterSignOutUrl='/' />
