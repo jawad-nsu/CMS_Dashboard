d396af17e1f7bf555eae9aa15b5d711788c85e7b
jawad-nsu <jawadurrahim1997@gmail.com>
2023-07-28 22:56:34 -0400

    add form to settings form

--- app/(dashboard)/[storeId]/(routes)/settings/settings-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/settings/settings-form.tsx
@@ -5,13 +5,25 @@ import { useForm } from 'react-hook-form';
 import { Store } from '@prisma/client';
 import { Trash } from 'lucide-react';
 import { zodResolver } from '@hookform/resolvers/zod';
+import { useParams, useRouter } from 'next/navigation';
 import * as z from 'zod';
+
+// api
+import { updateStore } from '@/backend/store';
 
 // components
 import Heading from '@/components/ui/heading';
 import { Button } from '@/components/ui/button';
 import { Separator } from '@/components/ui/separator';
-import { Form } from '@/components/ui/form';
+import {
+  Form,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormControl,
+  FormMessage,
+} from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
 
 interface SettingsFormProps {
   initialData: Store;
@@ -24,11 +36,16 @@ const formSchema = z.object({
 type SettingsFormValues = z.infer<typeof formSchema>;
 
 const SettingsForm: React.FC<SettingsFormProps> = ({ initialData }) => {
+  const params = useParams();
+  const router = useRouter();
   const [open, setOpen] = useState(false);
   const [loading, setLoading] = useState(false);
 
   const onSubmit = async (data: SettingsFormValues) => {
-    console.log(data);
+    try {
+      setLoading(true);
+      await updateStore(data, params.storeId);
+    } catch (err) {}
   };
 
   const form = useForm<SettingsFormValues>({
@@ -40,16 +57,44 @@ const SettingsForm: React.FC<SettingsFormProps> = ({ initialData }) => {
     <>
       <div className='flex items-center justify-between'>
         <Heading title='Settings' description='Manage store preferences' />
-        <Button variant='destructive' size='sm' onClick={() => {}}>
+        <Button
+          disabled={loading}
+          variant='destructive'
+          size='sm'
+          onClick={() => setOpen(true)}
+        >
           <Trash className='h-4 w-4' />
         </Button>
       </div>
       <Separator />
-      <Form>
+      <Form {...form}>
         <form
           onSubmit={form.handleSubmit(onSubmit)}
           className='space-y-8 w-full'
-        ></form>
+        >
+          <div className='grid grid-cols-3 gap-8'>
+            <FormField
+              control={form.control}
+              name='name'
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Name</FormLabel>
+                  <FormControl>
+                    <Input
+                      disabled={loading}
+                      placeholder='Store name'
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+          <Button disabled={loading} className='ml-auto' type='submit'>
+            Save changes
+          </Button>
+        </form>
       </Form>
     </>
   );
