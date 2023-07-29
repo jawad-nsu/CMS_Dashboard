db6ae12380232c1b70ffed0589f0fc56b4ed1927
jawad-nsu <jawadurrahim1997@gmail.com>
2023-07-28 22:57:08 -0400

    add wrapper function to call updateStore API

--- backend/store.ts
+++ backend/store.ts
@@ -4,3 +4,8 @@ export async function postStore(values: Object) {
   const res = await axios.post('/api/stores', values);
   return res;
 }
+
+export async function updateStore(values: Object, storeId: String) {
+  const res = await axios.patch(`/api/stores/${storeId}`, values);
+  return res;
+}
